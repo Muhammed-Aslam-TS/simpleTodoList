0a5d30b4ce839d7429f6d29d60ebdcb338b5e35f
muhammed aslam <taslam8797@gmail.com>
2023-03-26 21:51:56 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Todo List';
+
+  list:any[]=[];
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list);
+  }
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+}
